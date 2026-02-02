6daab246af237339475e793de1f151511fe971db
amalkrishna0 <amalkrishna528@gmail.com>
2026-02-02 13:35:13 +0000

    project-19:updated s3 backup file

--- project-19-s3-backup-cron/s3_backup.sh
+++ project-19-s3-backup-cron/s3_backup.sh
@@ -1,29 +1,17 @@
-#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-
-
-# Backup important system logs
 
 LOG_DIR="/var/log"
-
-
-
 BUCKET_NAME="project19-log-backup-amalk"
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-
 BACKUP_FILE="logs_$TIMESTAMP.tar.gz"
 
+# Compress logs (ignore missing files)
+tar -czf /tmp/$BACKUP_FILE $LOG_DIR/*.log $LOG_DIR/*.log.* $LOG_DIR/*.gz 2>/dev/null
 
+# Upload to S3 (use full path)
+ /usr/local/bin/aws s3 cp /tmp/$BACKUP_FILE s3://$BUCKET_NAME/backups/
 
-# Compress logs
-
-tar -czf /tmp/$BACKUP_FILE $LOG_DIR/*.log $LOG_DIR/*.log.* $LOG_DIR/*.gz
-
-
-
-# Upload to S3
-
-aws s3 cp /tmp/$BACKUP_FILE s3://$BUCKET_NAME/backups/
-
-
+# Cleanup
+rm -f /tmp/$BACKUP_FILE
