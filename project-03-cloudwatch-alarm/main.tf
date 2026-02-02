ecd3111ef62cf314cea55495c14f607afb394925
amalkrishna0 <amalkrishna528@gmail.com>
2026-02-02 09:49:01 +0000

    project-3:updated terraform formating

--- project-03-cloudwatch-alarm/main.tf
+++ project-03-cloudwatch-alarm/main.tf
@@ -2,13 +2,58 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "cpu-test-vpc"
+  }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "cpu-test-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "cpu-test-public-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "cpu-test-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 resource "aws_instance" "cpu_test" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  key_name                    = "amal"
+  vpc_security_group_ids      = [aws_security_group.cpu_test_sg.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "project3-cpu-test"
@@ -18,7 +63,7 @@ resource "aws_instance" "cpu_test" {
 resource "aws_security_group" "cpu_test_sg" {
   name        = "cpu-test-sg"
   description = "Allow SSH and optional FTP"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     description = "SSH access"
